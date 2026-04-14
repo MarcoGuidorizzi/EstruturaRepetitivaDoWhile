d4bc1c2fccf2a1d83d97cf4e0f0381db413ef9c4
MARCO ANTONIO GUIDORIZZI COUTINHO DE SOUZA <62554313+MarcoGuidorizzi@users.noreply.github.com>
2026-04-14 16:50:43 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorTemperatura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double C, F;
+            char repetir;
+
+            do {
+                Console.WriteLine("Digite a temperatura em Celsius: ");
+                C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                F = 9.0 * C / 5.0 + 32.0;
+                Console.WriteLine("Temperatura equivalente em Fahrenheit: " + F.ToString("F1", CultureInfo.InvariantCulture));
+                Console.WriteLine("Deseja repetir? (s/n)");
+                repetir = char.Parse(Console.ReadLine());
+            } while (repetir == 's');
+
+            Console.ReadLine();
+        }
+    }
+}
